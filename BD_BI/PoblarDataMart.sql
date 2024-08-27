b64c9571c41d420f7c0ba882ac2d1615cd649f89
Kevin Rivera <kevinriveravergaray@gmail.com>
2024-08-26 20:29:31 -0500

    Poblar hechos_transacciones

--- BD_BI/PoblarDataMart.sql
+++ BD_BI/PoblarDataMart.sql
@@ -55,17 +55,47 @@ SELECT
 	dc.cliente_id,
 	t.sucursal_id,
 	t.tipo_transaccion_id,
-	SUM(monto),
-	COUNT(*)
+	t.monto,
+	c.saldo-t.monto AS 'saldo_antes',
+	c.saldo AS 'saldo_despues',
+	COUNT(t.id)
 FROM transacciones t
-INNER JOIN cuentas c ON c.id=numero_cuenta_origen_id
+INNER JOIN cuentas c ON c.id=t.numero_cuenta_destino_id
 INNER JOIN detalle_cuentas dc ON dc.cuenta_id=c.id
+WHERE t.tipo_transaccion_id='10'
 GROUP BY 
 	t.fecha_transaccion,
 	c.tipo_cuenta_id,
 	dc.cliente_id,
 	t.sucursal_id,
 	t.tipo_transaccion_id,
-	monto;
+	t.monto,
+	c.saldo
+UNION
+SELECT
+	t.fecha_transaccion,
+	c.tipo_cuenta_id,
+	dc.cliente_id,
+	t.sucursal_id,
+	t.tipo_transaccion_id,
+	t.monto,
+	c.saldo+t.monto AS 'saldo_antes',
+	c.saldo AS 'saldo_despues',
+	COUNT(t.id)
+FROM transacciones t
+INNER JOIN cuentas c ON c.id=t.numero_cuenta_origen_id
+INNER JOIN detalle_cuentas dc ON dc.cuenta_id=c.id
+WHERE t.tipo_transaccion_id='11'
+GROUP BY 
+	t.fecha_transaccion,
+	c.tipo_cuenta_id,
+	dc.cliente_id,
+	t.sucursal_id,
+	t.tipo_transaccion_id,
+	t.monto,
+	c.saldo;
 
+SELECT*FROM tipos_transaccion;
 SELECT*FROM tipos_cuenta;
+SELECT*FROM transacciones;
+SELECT*FROM cuentas;
