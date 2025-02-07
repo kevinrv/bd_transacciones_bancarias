1291f1e12eb6f69c9a9996a1a72051d156240626
Kevin Rivera <kevinriveravergaray@gmail.com>
2025-02-06 19:56:59 -0500

    update

--- Masterclass 06-02-2025/Masterclass 06-02-2025.sql
+++ Masterclass 06-02-2025/Masterclass 06-02-2025.sql
@@ -1,18 +1,5 @@
 USE dsrp_transacciones_bancarias;
 /*Introducción
-En esta masterclass, analizaremos un caso práctico basado en un modelo de datos de transacciones bancarias. Abordaremos buenas prácticas de diseño, optimización de consultas y estrategias de escalabilidad para mejorar el rendimiento de bases de datos a gran escala.
-1. Explicación del Modelo de Datos
-1.1. Entidades Principales
-El modelo de datos está compuesto por las siguientes tablas:
-Clientes: Contiene información personal y segmentación de los clientes.
-Cuentas: Registra las cuentas bancarias con información como saldo, fecha de apertura y estado.
-Transacciones: Almacena operaciones realizadas entre cuentas.
-Sucursales: Lista las sucursales del banco.
-Tipos de Cuenta y Tipos de Transacción: Catálogos de tipos de cuentas y tipos de transacciones disponibles.
-1.2. Relación entre las Tablas
-Cada cliente puede tener varias cuentas (relación 1 a N).
-Una cuenta puede estar asociada a varias transacciones.
-Cada transacción está relacionada con una sucursal y un tipo de transacción.
 2. Optimización del Modelo de Datos
 2.1. Normalización vs. Desnormalización
 Se recomienda normalizar hasta la 3FN para evitar redundancia de datos.
@@ -21,9 +8,53 @@ En escenarios de consultas analíticas, se pueden aplicar técnicas de desnormal
 Índices Clustered en claves primarias.
 Índices No Clustered en campos frecuentemente filtrados (e.g., numero_documento, numero_cuenta).
 Fill Factor ajustado según la tasa de inserción y actualización de datos.
+
+
+Tasa de Inserción/Actualización	Recomendación de Fill Factor
+Baja (consultas mayormente de lectura)	90-100%
+Moderada (mezcla de lecturas y escrituras)	80-90%
+Alta (frecuentes inserciones/actualizaciones)	70-80%
+Muy Alta (intenso flujo de datos, como registros de transacciones)	50-70%
+*/
+CREATE INDEX IX_transacciones_fecha
+ON transacciones (fecha_transaccion)
+WITH (FILLFACTOR = 80);
+
+/*
 2.3. Particionamiento de Datos
 Dividir tablas grandes como transacciones por rango de fechas.
 Considerar filegroups separados para mejorar el acceso a datos históricos.
+
+*/
+-- Crear filegroup para 2022
+ALTER DATABASE dsrp_transacciones_bancarias
+ADD FILEGROUP FG_Transacciones_2022;
+
+ALTER DATABASE dsrp_transacciones_bancarias
+ADD FILE (
+    NAME = 'Transacciones_2022',
+    FILENAME = 'C:\Data\Transacciones_2022.ndf'
+) TO FILEGROUP FG_Transacciones_2022;
+
+-- Crear filegroup para 2023
+ALTER DATABASE dsrp_transacciones_bancarias
+ADD FILEGROUP FG_Transacciones_2023;
+
+ALTER DATABASE dsrp_transacciones_bancarias
+ADD FILE (
+    NAME = 'Transacciones_2023',
+    FILENAME = 'C:\Data\Transacciones_2023.ndf'
+) TO FILEGROUP FG_Transacciones_2023;
+
+-- Filegroup predeterminado para datos actuales (2024-2025)
+ALTER DATABASE dsrp_transacciones_bancarias
+MODIFY FILEGROUP [PRIMARY] DEFAULT;
+
+
+CREATE PARTITION FUNCTION PF_TransaccionesPorFecha (DATE)
+AS RANGE LEFT FOR VALUES 
+('2022-12-31', '2023-12-31');
+/*
 
 3. Optimización de Consultas
 3.1. Uso de EXPLAIN y STATISTICS
@@ -43,25 +74,28 @@ Reemplazar SELECT * por columnas específicas.
 
 Optimizar JOIN con índices adecuados.*/
 SELECT 
-    c.nombre, 
+    c.nombres, 
     t.fecha_transaccion, 
     t.monto, 
     s.nombre AS sucursal
 FROM 
     clientes c
-JOIN 
-    cuentas cu ON c.cliente_id = cu.cliente_id
-JOIN 
-    transacciones t ON cu.numero_cuenta_id = t.numero_cuenta_origen_id
-JOIN 
-    sucursales s ON t.sucursal_id = s.sucursal_id
+INNER JOIN
+	detalle_cuentas dt ON dt.cliente_id=c.id
+INNER JOIN 
+    cuentas cu ON cu.id = dt.cuenta_id
+INNER JOIN 
+    transacciones t ON cu.id = t.numero_cuenta_origen_id
+INNER JOIN 
+    sucursales s ON t.sucursal_id = s.id
 WHERE 
-    t.fecha_transaccion BETWEEN '2024-01-01' AND '2024-12-31'
-    AND s.nombre = 'Sucursal Central';
+    t.fecha_transaccion BETWEEN '2022-01-01' AND '2024-12-31'
+    AND s.nombre = 'Sucursal Lima Centro';
 
+SELECT*FROM sucursales;
 -- Crear indices:
 CREATE INDEX IX_cuentas_cliente
-ON cuentas (cliente_id);
+ON detalle_cuentas (cliente_id);
 
 
 /*
